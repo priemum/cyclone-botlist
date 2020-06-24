2fa20098594b41aae38dd0311dcbf59039736fb6
AhmedSabry957 <63425800+AhmedSabry957@users.noreply.github.com>
2020-06-24 20:00:10 +0200

    Update guildMemberAdd.js

--- src/bot/events/guildMemberAdd.js
+++ src/bot/events/guildMemberAdd.js
@@ -3,8 +3,9 @@ const { Event } = require('klasa');
 module.exports = class extends Event {
     run(member) {
         if (member.user.bot) {
-            member.roles.add(member.guild.roles.cache.get(process.env.BOT_ROLE_ID));
             member.roles.add(member.guild.roles.cache.get(process.env.UNVERIFIED_ROLE_ID));
+        }else{
+           member.roles.add(member.guild.roles.cache.get(process.env.USER_ROLE));
         }
     }
 };
