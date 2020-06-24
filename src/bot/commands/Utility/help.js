d31bb6cfa271d28c10feef7f50332b6be36417ff
AhmedSabry957 <63425800+AhmedSabry957@users.noreply.github.com>
2020-06-24 22:23:26 +0200

    Update help.js

--- src/bot/commands/Utility/help.js
+++ src/bot/commands/Utility/help.js
@@ -3,6 +3,7 @@ const { Command } = require('klasa');
 module.exports = class extends Command {
     constructor(...args) {super(...args, {});}
     async run(message) {
+        if(!message.member.roles.cache.get("725406861515292673")) return;
         let embed = new Discord.MessageEmbed()
         .setTitle("Cyclone Bot List ™")
         .setDecription("c!botinfo <@bot> ``Get bot info``\nc!count ``Tells you bots counts``\nc!marknsfw <@bot> ``Make bot nsfw``\nc!notnsfw <@user> ``unmark bot nsfw``\nc!queue ``Shows bot queue``\nc!remove <@bot> ``Remove bot from list``\nc!verify <@bot> ``Approve Bots!``\nc!widget <@bot>``Shows Bot widget``\nc!prune <amount> ``Delete amount of messages``\nc!ping ``Gives bot respond ms``")
