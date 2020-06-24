88b83363a7c587db3ac1a588be8883cbb174c8d6
AhmedSabry957 <63425800+AhmedSabry957@users.noreply.github.com>
2020-06-24 22:22:20 +0200

    Update help.js

--- src/bot/commands/Utility/help.js
+++ src/bot/commands/Utility/help.js
@@ -5,7 +5,7 @@ module.exports = class extends Command {
     async run(message) {
         let embed = new Discord.MessageEmbed()
         .setTitle("Cyclone Bot List ™")
-        .setDecription("c!botinfo <@bot> ``Get bot info``\nc!count ``Tells you bots counts``\nc!marknsfw <@bot> ``Make bot nsfw``\nc!notnsfw <@user> ``unmark bot nsfw``\nc!queue ``Shows bot queue``\nc!remove <@bot> ``Remove bot from list``\nc!verify <@bot> ``Approve Bots!``\nc!widget <@bot>``Shows Bot widget``")
+        .setDecription("c!botinfo <@bot> ``Get bot info``\nc!count ``Tells you bots counts``\nc!marknsfw <@bot> ``Make bot nsfw``\nc!notnsfw <@user> ``unmark bot nsfw``\nc!queue ``Shows bot queue``\nc!remove <@bot> ``Remove bot from list``\nc!verify <@bot> ``Approve Bots!``\nc!widget <@bot>``Shows Bot widget``\nc!prune <amount> ``Delete amount of messages``\nc!ping ``Gives bot respond ms``")
         .setFooter("Cyclone Bot List ™ || Help Command")
         }
     message.channel.send(embed)
