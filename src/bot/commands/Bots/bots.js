8a766b8d48a547cdf34db2957345164f42f1cbb6
AhmedSabry957 <63425800+AhmedSabry957@users.noreply.github.com>
2020-06-24 21:35:37 +0200

    Update bots.js

--- src/bot/commands/Bots/bots.js
+++ src/bot/commands/Bots/bots.js
@@ -12,26 +12,21 @@ module.exports = class extends Command {
     async run(message, [user]) {
         let person = user ? user : message.author;
 
-        let bots = await Bots.findOne({ 
-            botid: person.id }, { _id: false })
-        bots = bots.cache.filter(bot => bot.state !== "deleted" && bot.owners.includes(person.id));
-
-        if (bots.length === 0) return message.channel.send(`\`${person.tag}\` has no bots. Add one: <${process.env.DOMAIN}/add/>.`)
-        var cont = ``
-        var un = false;
-        for (let i = 0; i < bots.length; i++) {
-            let bot = bots[i];
-            if (bot.state == "unverified") {
-                un = true
-                cont += `~~<@${bot.id}>~~\n`
-            } else cont += `<@${bot.id}>\n`
-        }
-        let e = new MessageEmbed()
-            .setTitle(`${person.username}#${person.discriminator}'s bots`)
-            .setDescription(cont)
-            .setColor(0x6b83aa)
-        if (un) e.setFooter(`Bots with strikethrough are unverified.`)
-        message.channel.send(e)
+       if(!person) return message.reply("Please ping user")
+       Bots.find({
+    botid: person.id
+  }).sort([
+    ['descending']
+  ]).exec((err, res) => {
+           let embed = new MessageEmbed()
+            if (res.length === 0) return message.channel.send(`\`${person.tag}\` has no bots. Add one: <${process.env.DOMAIN}/add/>.`)
+           for (i = 0; i < res.length; i++){
+               embed.addField(`{i + 1}. <@${res.id}>`
+           }
+       })
+           embed.setTitle(`${person.username}#${person.discriminator}'s bots`)
+           embed.setColor(0x6b83aa)
+        message.channel.send(embed)
     }
 
 };
