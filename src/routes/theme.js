00da9df2e442a84fef8a8d6a88d18fc60978be18
AhmedSabry957 <63425800+AhmedSabry957@users.noreply.github.com>
2020-06-25 14:57:59 +0200

    Update theme.js

--- src/routes/theme.js
+++ src/routes/theme.js
@@ -8,7 +8,7 @@ route.get("/", async (req, res, next) => {
     let theme = req.cookies["theme"]
     if (!theme) return res.sendFile(path.resolve(__dirname + "/../Dynamic/theme/light.css"))
     else if (theme === "dark") return res.sendFile(path.resolve(__dirname + "/../Dynamic/theme/dark.css"));
-    else return res.sendFile(path.resolve(__dirname + "/../Dynamic/theme/dark.css"))
+    else return res.sendFile(path.resolve(__dirname + "/../Dynamic/theme/light.css"))
 });
 
 module.exports = route;
