76a2c0bc8b610d83b1bcd1d4eff745ad9ceccd1d
AhmedSabry957 <63425800+AhmedSabry957@users.noreply.github.com>
2020-06-25 11:52:15 +0200

    Update help.js

--- src/bot/commands/Utility/help.js
+++ src/bot/commands/Utility/help.js
@@ -1,13 +1,23 @@
-// This is just to disable automatic help command
 const { Command } = require('klasa');
+
 module.exports = class extends Command {
-    constructor(...args) {super(...args, {});}
-    async run(message) {
-        if(!message.member.roles.cache.get("725406861515292673")) return;
+    constructor(...args) {
+        super(...args, {
+            aliases: ["commands"],
+            usage: "[User:user]"
+        });
+    }
+
+    async run(message, [user]) {
         let embed = new Discord.MessageEmbed()
         .setTitle("Cyclone Bot List ™")
-        .setDecription("c!botinfo <@bot> ``Get bot info``\nc!count ``Tells you bots counts``\nc!marknsfw <@bot> ``Make bot nsfw``\nc!notnsfw <@user> ``unmark bot nsfw``\nc!queue ``Shows bot queue``\nc!remove <@bot> ``Remove bot from list``\nc!verify <@bot> ``Approve Bots!``\nc!widget <@bot>``Shows Bot widget``\nc!prune <amount> ``Delete amount of messages``\nc!ping ``Gives bot respond ms``")
-        .setFooter("Cyclone Bot List ™ || Help Command")
+          if(message.member.roles.cache.get("725406861515292673")){
+              embed.addField("Commands", "c!botinfo <@bot> ``Get bot info``\nc!count ``Tells you bots counts``\nc!marknsfw <@bot> ``Make bot nsfw``\nc!notnsfw <@user> ``unmark bot nsfw``\nc!queue ``Shows bot queue``\nc!remove <@bot> ``Remove bot from list``\nc!verify <@bot> ``Approve Bots!``\nc!widget <@bot>``Shows Bot widget``\nc!prune <amount> ``Delete amount of messages``\nc!ping ``Gives bot respond ms``")
+          }else{
+        embed.addField("Commands", "c!ping ``Shows bot latency``\nc!widget <@bot> ``Shows bot widget(Embed)``\nc!botinfo <@bot> ``Shows bot info``\nc!count")
+          }
+        embed.setFooter("Cyclone Bot List ™ || Help Command")
         }
     message.channel.send(embed)
+    }
 };
