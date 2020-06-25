830e8030aae31a9719d916c1c99cc9f36fb14ba2
AhmedSabry957 <63425800+AhmedSabry957@users.noreply.github.com>
2020-06-25 23:18:40 +0200

    Update resubmit.js

--- src/routes/bots/resubmit.js
+++ src/routes/bots/resubmit.js
@@ -5,12 +5,12 @@ const route = Router();
 
 route.get("/:id", async (req, res, next) => {
    Bots.findOne({
-        botid: req.params.id}, (err,res) => {
-        if (!res) return res.sendStatus(404);
-       if (res.state !== "deleted") return res.sendStatus(404);
-        res.state = 'unverified'
-        res.save
-                res.render("resubmit/index", { bot: res });
+        botid: req.params.id}, (err,bot) => {
+        if (!bot) return res.sendStatus(404);
+       if (bot.state !== "deleted") return res.sendStatus(404);
+        bot.state = 'unverified'
+        bot.save
+                res.render("resubmit/index", { bot: bot });
     })
 });
 
