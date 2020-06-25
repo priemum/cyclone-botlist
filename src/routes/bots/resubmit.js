ea4703c6b09d66bb6a68ffa172f96e680ff83f68
AhmedSabry957 <63425800+AhmedSabry957@users.noreply.github.com>
2020-06-25 23:47:07 +0200

    Update resubmit.js

--- src/routes/bots/resubmit.js
+++ src/routes/bots/resubmit.js
@@ -4,15 +4,11 @@ const Bots = require("@models/bots");
 const route = Router();
 
 route.get("/:id", async (req, res, next) => {
-    let clientid = req.params.value;
-    Bots.findOne({
-        botid: clientid}, (err , bot) => {
+    let clientid = req.params.id;
+    let bot = await Bots.updateOne({ botid: clientid }, { $set: { state: "unverified" } })
     if (!bot) return res.sendStatus(404);
     if (bot.state !== 'deleted') return res.sendStatus(404);
     res.render("resubmit/index", { bot: bot });
-        bot.state = 'unverified'
-        bot.save()
-    })
 });
 
 module.exports = route;
