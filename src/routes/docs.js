40ccf64368189ba6a61309cffbd7131fcb8c2458
AhmedSabry957 <63425800+AhmedSabry957@users.noreply.github.com>
2020-06-25 16:40:18 +0200

    Update docs.js

--- src/routes/docs.js
+++ src/routes/docs.js
@@ -4,7 +4,7 @@ const { GUILD_INVITE } = process.env;
 const route = Router();
 
 route.get("/", async (req, res, next) => {
-       res.render("user/me", data);
+       res.render("/docs", docs);
 });
 
 module.exports = route;
