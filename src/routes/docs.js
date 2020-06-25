181d6176a6d7daade80a763d47e0426adbcae418
AhmedSabry957 <63425800+AhmedSabry957@users.noreply.github.com>
2020-06-25 16:32:04 +0200

    Update docs.js

--- src/routes/docs.js
+++ src/routes/docs.js
@@ -1,21 +1,10 @@
 const { Router } = require("express");
-const Bots = require("@models/bots");
+const { GUILD_INVITE } = process.env;
 
 const route = Router();
 
-route.get("/:id", async (req, res, next) => {
-    let user = req.app.get('client').users.cache.get(req.params.id);
-    if (!user) return res.render("user/notfound", {})
-
-    let bots = await Bots.find({}, { _id: false })
-    bots = bots.filter(bot => bot.owners.includes(user.id))
-    
-    if (bots.length === 0) return res.render("user/notfound", {})
-    let data = {
-        user: user,
-        cards: bots
-    }
-    res.render("user/index", data);
+route.get("/", async (req, res, next) => {
+    res.redirect(GUILD_INVITE)
 });
 
 module.exports = route;
