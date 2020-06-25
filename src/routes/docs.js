82230f0ee7b249923a7d3b1006eb86c53ade65fb
AhmedSabry957 <63425800+AhmedSabry957@users.noreply.github.com>
2020-06-25 16:42:10 +0200

    Update docs.js

--- src/routes/docs.js
+++ src/routes/docs.js
@@ -3,7 +3,7 @@ const { Router } = require("express");
 const route = Router();
 
 route.get("/", async (req, res, next) => {
-       res.render("/docs", docs);
+       res.render("docs");
 });
 
 module.exports = route;
