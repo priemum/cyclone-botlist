f122a302c13fff41d2a1b287572b0d9acc7bca40
AhmedSabry957 <63425800+AhmedSabry957@users.noreply.github.com>
2020-06-25 20:02:18 +0200

    Update theme.js

--- src/routes/api/theme.js
+++ src/routes/api/theme.js
@@ -8,6 +8,13 @@ route.get("/", async (req, res, next) => {
         res.cookie("theme", "dark");
         return res.redirect(req.header('Referer') || '/');
     } 
+    if (theme === "light") {
+            res.redirect(req.header('Referer') || '/');
+        return res.cookie("theme", "dark")
+    }else if(theme === "dark"){
+         res.redirect(req.header('Referer') || '/');
+        return res.cookie("theme", "light")
+    }
 });
 
 module.exports = route;
