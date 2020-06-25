a59a468373bccc56a2cf48a036f0011645b20bee
AhmedSabry957 <63425800+AhmedSabry957@users.noreply.github.com>
2020-06-25 19:44:23 +0200

    Update theme.js

--- src/routes/api/theme.js
+++ src/routes/api/theme.js
@@ -5,7 +5,7 @@ const route = Router();
 route.get("/", async (req, res, next) => {
     let theme = req.cookies["theme"]
     if (!theme) {
-        res.cookie("theme", "light");
+        res.cookie("theme", "dark");
         return res.redirect(req.header('Referer') || '/');
     } else if (theme === "light") {
         res.cookie("theme", "dark")
