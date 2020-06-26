1004852ee500644370729cc264534f9d0e4a1901
AhmedSabry957 <63425800+AhmedSabry957@users.noreply.github.com>
2020-06-26 14:46:19 +0200

    Create vote.js

---
+++ src/public/assets/js/vote.js
@@ -0,0 +1,30 @@
+function votes() {
+    if (!document.getElementById('botid').value)
+        return flash(document.getElementById('botid'))
+   
+
+    let data = {
+        votes: 1,
+    };
+
+    fetch("/vote", {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers:{
+          'Content-Type': 'application/json'
+        }
+    }).then(res => res.json())
+    .then(response => {
+        location.href = response.redirect
+    })
+}
+
+function flash(element) {
+    element.scrollIntoView();
+    element.style.border = "2px solid #ff0000";
+    element.style.backgroundColor = "rgba(255, 0, 0, 0.5)";
+    setTimeout(() => {
+        element.style.backgroundColor = "rgba(0, 0, 0, 0)";
+        element.style.border = "1px solid #888";
+    }, 600)
+}
