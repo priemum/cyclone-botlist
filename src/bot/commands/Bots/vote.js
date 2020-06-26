dc65d12be259130821ed2ad533153fa2c5773fee
AhmedSabry957 <63425800+AhmedSabry957@users.noreply.github.com>
2020-06-26 20:47:57 +0200

    Update vote.js

--- src/bot/commands/Bots/vote.js
+++ src/bot/commands/Bots/vote.js
@@ -13,45 +13,44 @@ module.exports = class extends Command {
         });
     }
 
-    async run(message, [user]) {
-        if (!user || !user.bot) return message.channel.send(`Ping a **bot** to vote.`);
-                BotOnce.findOne({
-            botid: user.id,
-            voter: message.author.id}, async (err , res) => {
-            if(!res){
-                const newvoted = new BotOnce({
-                     botid: user.id,
-                     voter: message.author.id
-                })
-                newvoted.save()
-            }else{
-                return message.reply("You already voted for that bot")
-            }
-        let bot = await Bots.findOne({botid: user.id}, { _id: false })
-        Bots.findOne({
-          botid: user.id}, (err, res) => {
-            if(!res){
-           return message.reply("Not Exists bot!")
-         }else if(res.state == 'unverified'){
-                      return message.reply("That bot not verified to recieve votes!")
-         }
-            res.votes = res.votes- + -1
-            res.save()
-          return message.channel.send("Success voted for " + user.username)
-        })
-        let e = new MessageEmbed()
-            .setTitle('Vote Message')
-            .addField(`Bot`, `<@${user.id}>`, true)
-            .addField("Voter", message.author, true)
-            .setThumbnail(bot.logo)
-            .setTimestamp()
-            .setColor(0x26ff00)
-        modLog.send(e);
-        modLog.send(`${message.author}`).then(m => { m.delete() });
-    }
+async run(message, [user]) {
+if (!user || !user.bot) return message.channel.send(`Ping a **bot** to vote.`);
+BotOnce.findOne({
+botid: user.id,
+voter: message.author.id}, async (err , res) => {
+if(!res){
+const newvoted = new BotOnce({
+     botid: user.id,
+     voter: message.author.id
+})
+newvoted.save()
+}else{
+return message.reply("You already voted for that bot")
+}
+let bot = await Bots.findOne({botid: user.id}, { _id: false })
+Bots.findOne({
+botid: user.id}, (err, res) => {
+if(!res){
+return message.reply("Not Exists bot!")
+}else if(res.state == 'unverified'){
+      return message.reply("That bot not verified to recieve votes!")
+}
+res.votes = res.votes- + -1
+res.save()
+return message.channel.send("Success voted for " + user.username)
+})
+let e = new MessageEmbed()
+.setTitle('Vote Message')
+.addField(`Bot`, `<@${user.id}>`, true)
+.addField("Voter", message.author, true)
+.setThumbnail(bot.logo)
+.setTimestamp()
+.setColor(0x26ff00)
+modLog.send(e);
+modLog.send(`${message.author}`).then(m => { m.delete() });
 
-    async init() {
-        modLog = this.client.channels.cache.get(process.env.MOD_LOG_ID);
-    }
-                })
-};
+async init() {
+modLog = this.client.channels.cache.get(process.env.MOD_LOG_ID);
+}
+})
+}
