bfc78d8e9f2a861a1091f3abdd5e53550f965515
AhmedSabry957 <63425800+AhmedSabry957@users.noreply.github.com>
2020-06-26 16:33:04 +0200

    Update index.js

--- src/routes/bots/index.js
+++ src/routes/bots/index.js
@@ -21,11 +21,11 @@ route.get('/:id', async (req, res, next) => {
     let vote = await VoteBots.findOne({botid: req.params.id}, { _id: false, auth: false })
     if (!vote){
         let newvote = new VoteBots({
-            votes: 1,
+            votes: 0,
             botid: req.params.id
         })
         newvote.save()
-        return res.sendStatus(404);
+        vote.votes = 0
     }
     if (!bot) return res.sendStatus(404);
     if (bot.state === "deleted") return res.sendStatus(404);
