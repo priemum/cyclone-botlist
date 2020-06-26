d1c43fe2ec893b6eb650b6babab34087e733edf7
AhmedSabry957 <63425800+AhmedSabry957@users.noreply.github.com>
2020-06-26 13:48:06 +0200

    Update vote.js

--- src/public/assets/js/vote.js
+++ src/public/assets/js/vote.js
@@ -1,1 +1,18 @@
-test
+const bot = require("@models/vote")
+function votes(){
+         bot.findOne({
+         botid: bot.botid}, (err , res) => {
+         if(!res){
+         const newvote = new BLACKBOT({
+                votes: 1,
+                botid: bot.botid,
+            })
+            newvote.save().catch(err => console.log(err));
+            alert("Success voted for this bot")
+         }else{
+         res.votes = res.votes- + -1
+         res.save()
+         alert("Success voted for this bot")
+         }
+   })
+}
