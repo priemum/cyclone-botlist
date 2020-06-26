aa6714b5e0eb9473cdb05fa8d6f8353660bd9fc4
AhmedSabry957 <63425800+AhmedSabry957@users.noreply.github.com>
2020-06-26 19:58:43 +0200

    Delete vote.js

--- src/models/vote.js
+++
@@ -1,9 +0,0 @@
-  
-const mongoose = require("mongoose");
-
-const usersSchema = new mongoose.Schema({
-  votes: Number,
-  botid: String
-});
-
-module.exports = mongoose.model("Votes", usersSchema)
