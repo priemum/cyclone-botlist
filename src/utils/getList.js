4f7f67e88de78d1020682465c5636460e5046e08
AhmedSabry957 <63425800+AhmedSabry957@users.noreply.github.com>
2020-06-26 19:31:07 +0200

    Update getList.js

--- src/utils/getList.js
+++ src/utils/getList.js
@@ -1,6 +1,6 @@
 const Bots = require("@models/bots");
 
 module.exports = async () => {
-const bots = await Bots.find({}, { _id: false, auth: false }).sort([['descending']])
- return bots.filter(bot => bot.state != "deleted")
+const bots = await Bots.find({}, { _id: false, auth: false })
+ return bots.filter(bot => bot.state != "deleted").sort([['descending']])
 };
