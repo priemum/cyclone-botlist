a788875f308536bee6174b8316f7c1139f60adbb
priemum <50713962+priemum@users.noreply.github.com>
2021-10-11 12:38:50 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 const colors = require('colors');
 const bot = require('@bot/index');
-const App = require('@structures/app.js');
+const App = require('@structures/app.js');  
 const { PORT, DISCORD_TOKEN, MONGO_DB_URL } = process.env;
 
 
